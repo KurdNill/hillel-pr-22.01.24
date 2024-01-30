a3e22fbdd4bd7542f90e5ba57e4b56049a3dc91a
KurdNill <forwardnil2021@gmail.com>
2024-01-30 14:17:18 +0200

    new commit

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
 <?php
-
-declare(strict_types=1);
-
-require_once 'configs.php';
-require_once ROOT . 'classes/Animal.php';
-require_once ROOT . 'classes/Cat.php';
-require_once ROOT . 'classes/Calculator.php';
-require_once ROOT . 'classes/ValueObject.php';
-
-
-
-$dsn = 'mysql:host=mysql_db;dbname=php_advanced';
-$pdo = new PDO($dsn, 'root', 'secret');
-
-var_dump($pdo);
-
-echo 1;
