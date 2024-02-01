17b1a70fec6cee14534d78e9cc600aca8b94f907
KurdNill <157412384+KurdNill@users.noreply.github.com>
2024-02-01 21:42:00 +0200

    hw-2

--- index.php
+++ index.php
@@ -7,5 +7,8 @@ require_once ROOT . 'classes/Animal.php';
 require_once ROOT . 'classes/Cat.php';
 //require_once ROOT . 'classes/Calculator.php';
 //require_once ROOT . 'classes/ValueObject.php';
+require_once ROOT . 'vendor/autoload.php';
 
-
+$dsn = "mysql:host=mysql_db;dbname=php_advanced";
+$pdo = new PDO($dsn, 'root', 'secret');
+var_dump($pdo);
