5eb93c8e03cb75d6feb16c8da6e8630605bbe2f2
KurdNill <157412384+KurdNill@users.noreply.github.com>
2024-02-09 14:54:20 +0200

    hw-8

---
+++ classes/ManipulateProduct.php
@@ -0,0 +1,8 @@
+<?php
+
+class ManipulateProduct
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+}
---
+++ classes/Product.php
@@ -0,0 +1,9 @@
+<?php
+
+class Product
+{
+    private array $name = [];
+    public function get(string $name) {}
+    public function set(string $name, string $value) {}
+
+}
